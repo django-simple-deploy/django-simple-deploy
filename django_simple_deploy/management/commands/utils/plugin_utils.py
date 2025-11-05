15f24ae6529a44a6f5d63fd112fe3fd5f1aecf29
eric <eric@example.com>
2025-11-05 16:43:02 -0500

    Catch decoding error in run_slow_command().

--- django_simple_deploy/management/commands/utils/plugin_utils.py
+++ django_simple_deploy/management/commands/utils/plugin_utils.py
@@ -216,8 +216,18 @@ def run_slow_command(cmd, skip_logging=False):
         universal_newlines=True,
         shell=dsd_config.use_shell,
     ) as p:
-        for line in p.stderr:
-            write_output(line, skip_logging=skip_logging)
+        # This is a bit ugly, but it came up when troubleshooting dsd-upsun on Windows.
+        # I'm hesistant to rewrite this significantly without testing other plugins.
+        # Would it work to build a TextIOWrapper instance with encoding="utf-8"?
+        # I'm not sure why this has worked for other plugins on Windows, but not for dsd-upsun.
+        # Most Windows testing has been in a VM on macOS.
+        try:
+            for line in p.stderr:
+                write_output(line, skip_logging=skip_logging)
+        except UnicodeDecodeError:
+            p.encoding="utf-8"
+            for line in p.stderr:
+                write_output(line, skip_logging=skip_logging)
 
     if p.returncode != 0:
         raise subprocess.CalledProcessError(p.returncode, p.args)
