071c7ac71934fe3f1f4f94a6dc01e91d74f37e4d
Eric Matthes <ehmatthes@gmail.com>
2025-11-08 10:14:18 -0500

    Only pull msg from pre_inspect return value if it exists.

--- django_simple_deploy/management/commands/deploy.py
+++ django_simple_deploy/management/commands/deploy.py
@@ -146,7 +146,8 @@ class Command(BaseCommand):
 
         # Allow the plugin to do pre-inspection work. See note in hook spec;
         # this should be used sparingly.
-        if (msg_pre_inspect := pm.hook.dsd_pre_inspect()[0]):
+        if (msgs := pm.hook.dsd_pre_inspect()):
+            msg_pre_inspect = msgs[0]
             plugin_utils.write_output(msg_pre_inspect)
 
         platform_name = self.plugin_config.platform_name
