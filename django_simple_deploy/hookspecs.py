55d7f55397ee2074e4731d89665a09b14af78ab5
Eric Matthes <ehmatthes@gmail.com>
2025-11-08 09:29:49 -0500

    Hook spec for pre-inspection work by plugin.

--- django_simple_deploy/hookspecs.py
+++ django_simple_deploy/hookspecs.py
@@ -25,5 +25,26 @@ def dsd_validate_cli(options):
     """Validate the plugin-specific CLI args."""
 
 @hookspec
+def dsd_pre_inspect():
+    """Allow plugin to do work before core inspects the user's project.
+
+    You should only use this hook if there's something that *must* happen
+    before core inspects the user's project. We want to do all the core work
+    before the plugin modifies the project. We want the chance to find any
+    reasons we'll have to bail before making configuration changes.
+
+    The motivation for this hook is that a plugin needed to address a bug in how
+    a platform's CLI mishandled Git exclude files. If the plugin can't do
+    pre-inspection work, that plugin will never pass the git status check that
+    core runs before handing off to the plugin. This hook allows that plugin
+    to address a bug that the upstream CLI hasn't yet addressed. This is
+    exactly the kind of buffer we want to be, between the user and the platform.
+
+    Please use this hook sparingly, if at all.
+
+    See: https://github.com/django-simple-deploy/django-simple-deploy/issues/466
+    """
+
+@hookspec
 def dsd_deploy():
     """Carry out all platform-specific configuration and deployment work."""
